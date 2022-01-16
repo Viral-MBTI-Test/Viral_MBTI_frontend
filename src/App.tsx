a0e1c48c13777170ad19ba873c28c3ecfd6a3902
rlafl7942 <rlafl7942@naver.com>
2022-01-16 22:29:21 +0900

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import Login from './login/components/login';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Query from './query/query';
-import Queryend from './query/queryend';
-import SetInfo from './setInfo/setInfo';
-import Result from './result/result';
+import Login from "./login/components/login";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Query from "./query/query";
+import Queryend from "./query/queryend";
+import SetInfo from "./setInfo/setInfo";
+import Result from "./result/result";
 function App() {
-  return (
-    <div className="app">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/setinfo" element={<SetInfo name="d" />} />
-          <Route path="/question/0" element={<Query />} />
-          <Route path="/question/0/queryend" element={<Queryend />} />
-          <Route path="/result" element={<Result />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/setinfo" element={<SetInfo />} />
+                    <Route path="/question/0" element={<Query />} />
+                    <Route path="/question/0/queryend" element={<Queryend />} />
+                    <Route path="/result" element={<Result />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
