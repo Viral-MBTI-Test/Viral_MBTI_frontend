5f20128805a2c420b717b903271f7424490bf850
rlafl7942 <rlafl7942@naver.com>
2022-01-22 23:14:07 +0900

    add left part in result

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,52 @@
-import Login from './login/components/login';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Query from './query/query';
-import Queryend from './query/queryend';
-import SetInfo from './setInfo/setInfo';
-import Result from './result/result';
-import Auth from './login/components/auth';
-import FriendsAuth from './login/components/friendsAuth';
-import { useState } from 'react';
-import user_empty from '../images/user_empty.svg';
+import Login from "./login/components/login";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Query from "./query/query";
+import Queryend from "./query/queryend";
+import SetInfo from "./setInfo/setInfo";
+import Result from "./result/result";
+import Auth from "./login/components/auth";
+import FriendsAuth from "./login/components/friendsAuth";
+import { useState } from "react";
+import user_empty from "../images/user_empty.svg";
 
 function App() {
-  const [username, setUsername] = useState<string>('동의하기');
-  const [profileImage, setProfileImage] = useState<string>(
-    'https://i.ibb.co/km2c6Zy/Frame-44.png'
-  );
+    const [username, setUsername] = useState<string>("동의하기");
+    const [profileImage, setProfileImage] = useState<string>(
+        "https://i.ibb.co/km2c6Zy/Frame-44.png"
+    );
 
-  return (
-    <div className="app">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route
-            path="/friendsAuth"
-            element={
-              <FriendsAuth
-                setUsername={setUsername}
-                setProfileImage={setProfileImage}
-              />
-            }
-          />
-          <Route
-            path="/setinfo"
-            element={
-              <SetInfo username={username} profileImage={profileImage} />
-            }
-          />
-          <Route path="/question/0" element={<Query />} />
-          <Route path="/question/0/queryend" element={<Queryend />} />
-          <Route path="/result" element={<Result />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/auth" element={<Auth />} />
+                    <Route
+                        path="/friendsAuth"
+                        element={
+                            <FriendsAuth
+                                setUsername={setUsername}
+                                setProfileImage={setProfileImage}
+                            />
+                        }
+                    />
+                    <Route
+                        path="/setinfo"
+                        element={
+                            <SetInfo
+                                username={username}
+                                profileImage={profileImage}
+                            />
+                        }
+                    />
+                    <Route path="/question/0" element={<Query />} />
+                    <Route path="/question/0/queryend" element={<Queryend />} />
+                    <Route path="/result" element={<Result />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/result/result.tsx
+++ src/result/result.tsx
@@ -1,5 +1,18 @@
 import React, { useState } from "react";
 import "./result.css";
+import boy from "../images/boy.svg";
+
+interface personalityResultProps {
+    frequency: string;
+    result: string[];
+}
+
+interface similarFriendsProps {
+    photo: string;
+    name: string;
+    mbtiExplain: string;
+    percentage: number;
+}
 
 const Result = () => {
     return (
@@ -25,51 +38,133 @@ const Result = () => {
                 <div className="result_title_small">
                     나는 이런 성격을 갖고 있어요
                 </div>
-                <div className="result_rectangle_result_box">
-                    <div className="result_personality_text">
-                        📍 밖에 놀러가는 것도 좋지만, 집에서 쉬는 것도 좋아해요
-                        좋아좋아 좋아해~~
-                    </div>
-                    <div className="result_personality_text">
-                        📍 밖에 놀러가는 것도 좋지만, 집에서 쉬는 것도 좋아해요
-                        좋아좋아 좋아해~~
-                    </div>
-                    <div className="result_personality_text">
-                        📍 밖에 놀러가는 것도 좋지만, 집에서 쉬는 것도 좋아해요
-                        좋아좋아 좋아해~~
-                    </div>
-                </div>
+                <RectangleResult
+                    frequency="often"
+                    result={[
+                        "밖에 놀러가는 것도 좋지만, 집에서 쉬는 것도 좋아해요 좋아좋아 좋아해~~",
+                        "2",
+                        "밖에 놀러가는 것도 좋지만, 집에서 쉬는 것도 좋아해요 좋아좋아 좋아해~~",
+                        "2",
+                        "밖에 놀러가는 것도 좋지만, 집에서 쉬는 것도 좋아해요 좋아좋아 좋아해~~",
+                        "2",
+                    ]}
+                />
             </div>
             <div className="result_rectangle_big_container">
                 <div className="result_title_small">가끔은...</div>
-                <div className="result_rectangle_result_box">
-                    <div className="result_personality_text">
-                        ✅ 내 의견과 상대 의견이 다르면 설득하려는 스타일이에요
-                    </div>
-                    <div className="result_personality_text">
-                        ✅ 내 의견과 상대 의견이 다르면 설득하려는 스타일이에요
-                    </div>
-                    <div className="result_personality_text">
-                        ✅ 내 의견과 상대 의견이 다르면 설득하려는 스타일이에요
-                    </div>
-                </div>
+                <RectangleResult
+                    frequency="sometimes"
+                    result={[
+                        "내 의견과 상대 의견이 다르면 설득하려는 스타일이에요",
+                        "2",
+                    ]}
+                />
             </div>
-            <div className="result_rectangle_big_container">
-                <div className="result_title_small">가끔은...</div>
-                <div className="result_rectangle_result_box">
-                    <div className="result_personality_text">
-                        ✅ 내 의견과 상대 의견이 다르면 설득하려는 스타일이에요
-                    </div>
-                    <div className="result_personality_text">
-                        ✅ 내 의견과 상대 의견이 다르면 설득하려는 스타일이에요
-                    </div>
-                    <div className="result_personality_text">
-                        ✅ 내 의견과 상대 의견이 다르면 설득하려는 스타일이에요
-                    </div>
+            <div className="result_myFriendsContainer">
+                <span>나와 비슷한 성향의 친구들</span>
+                <div style={{ height: "20px" }} />
+                <SimilarFriends />
+                <SimilarFriends />
+                <SimilarFriends />
+            </div>
+            <div className="result_buttonContainer">
+                <div className="result_button">전체 친구목록</div>
+                <div className="result_button">질문별 결과 보기</div>
+            </div>
+            <div className="result_shareContainer">
+                <span>결과 공유하기</span>
+                <div className="result_shareIcons">
+                    <img
+                        src={boy}
+                        alt="boy"
+                        style={{
+                            width: "40px",
+                            height: "40px",
+                            backgroundColor: "black",
+                            borderRadius: "15px",
+                        }}
+                    />
+                    <img
+                        src={boy}
+                        alt="boy"
+                        style={{
+                            width: "40px",
+                            height: "40px",
+                            backgroundColor: "black",
+                            borderRadius: "15px",
+                        }}
+                    />
+                    <img
+                        src={boy}
+                        alt="boy"
+                        style={{
+                            width: "40px",
+                            height: "40px",
+                            backgroundColor: "black",
+                            borderRadius: "15px",
+                        }}
+                    />
+                    <img
+                        src={boy}
+                        alt="boy"
+                        style={{
+                            width: "40px",
+                            height: "40px",
+                            backgroundColor: "black",
+                            borderRadius: "15px",
+                        }}
+                    />
                 </div>
             </div>
         </div>
     );
+};
+
+const SimilarFriends = () => {
+    return (
+        <div className="result_friendProfile">
+            <img
+                src={boy}
+                alt="boy"
+                style={{
+                    width: "40px",
+                    height: "40px",
+                    backgroundColor: "black",
+                    borderRadius: "15px",
+                }}
+            />
+            <div className="result_profileText">
+                <span>김진형</span>
+                <p>ENTP에 INTJ 한스푼</p>
+            </div>
+            <div className="result_friendProfilePercent">
+                <span>55%</span>
+            </div>
+        </div>
+    );
+};
+
+const RectangleResult = (props: personalityResultProps) => {
+    return (
+        <div className="result_rectangle_result_box">
+            {props.result.map((result, index) => {
+                return (
+                    <ResultText
+                        key={index}
+                        result={
+                            props.frequency === "often"
+                                ? `📍 ${result}`
+                                : `✅ ${result}`
+                        }
+                    />
+                );
+            })}
+        </div>
+    );
+};
+
+const ResultText = (props: { result: string }) => {
+    return <div className="result_personality_text">{props.result}</div>;
 };
 
 const RectangleSmall = (mbtiPercentProps: {
@@ -88,6 +183,7 @@ const RectangleSmall = (mbtiPercentProps: {
             style={{
                 backgroundColor: `${color[mbtiPercentProps.index]}`,
                 marginTop: mbtiPercentProps.index === 0 ? "16px" : "0px",
+                height: mbtiPercentProps.index === 0 ? "24px" : "20px",
             }}
         >
             <span
