446931c150072c58f845b719cb98c5c1b274cdcc
RightHennessy <myheaven_b@naver.com>
2022-01-24 17:57:52 +0900

    fix : not solve - first query

--- src/query/query.tsx
+++ src/query/query.tsx
@@ -1,17 +1,17 @@
 import './query.css';
 import {useNavigate} from 'react-router-dom';
-/*import { Link } from 'react-router-dom';*/
 import { Querylist } from './querylist';
 import {useState} from "react";
-
+/*import webClient from '../../share/webClient';
+import { AxiosResponse } from 'axios';
+*/
 const Query = () => {
-  const navigate = useNavigate();
 
+  const navigate = useNavigate();
   const [currentNo, setCurrentNo] = useState(0);
 
   const answerClick = () => {
     if (currentNo === Querylist.length - 1) {
-      /* <Link to="/result" style={{ textDecoration: 'none' }}> </Link> */
       navigate("/result");
     } else {
       setCurrentNo((currentNo) => currentNo + 1);
@@ -19,6 +19,9 @@ const Query = () => {
   };
 
   const beforeClick = () => {
+    if(currentNo === 0){
+      alert("첫질문 -> 예외처리")
+    }
     setCurrentNo((currentNo) => currentNo - 1);
   }
 
