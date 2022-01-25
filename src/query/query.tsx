0592976b5c8ced08b6f44e267e49154121a2d597
RightHennessy <myheaven_b@naver.com>
2022-01-25 09:41:54 +0900

    fix : remove first page prevBtn

--- src/query/query.tsx
+++ src/query/query.tsx
@@ -2,32 +2,37 @@ import './query.css';
 import {useNavigate} from 'react-router-dom';
 import { Querylist } from './querylist';
 import {useState} from "react";
-/*import webClient from '../../share/webClient';
-import { AxiosResponse } from 'axios';
-*/
+import ProgressBar from 'react-bootstrap/ProgressBar'
+
 const Query = () => {
 
   const navigate = useNavigate();
   const [currentNo, setCurrentNo] = useState(0);
+  const [show, setShow] = useState(false);
 
   const answerClick = () => {
     if (currentNo === Querylist.length - 1) {
       navigate("/result");
     } else {
+      setShow(true);
       setCurrentNo((currentNo) => currentNo + 1);
     }
   };
 
   const beforeClick = () => {
-    if(currentNo === 0){
-      alert("첫질문 -> 예외처리")
+    if(currentNo-1 === 0 || currentNo === 0){
+      setShow(false);
     }
     setCurrentNo((currentNo) => currentNo - 1);
   }
 
   return (
-    <div className="query_container">
-      <div className="progress_div"></div>
+    <div className="query_container" >
+      <div className="progress_div">
+        <div className="progress_state">
+          <ProgressBar now={currentNo} />
+        </div>
+      </div>
 
       <div className="query_question">
         {Querylist[currentNo].question}
@@ -39,8 +44,10 @@ const Query = () => {
         ))}
       </div>
      
-      <div className="query_prevBtn" onClick={beforeClick}>이전 질문</div>
-     
+      { show && <div>
+      <div className="query_prevBtn" onClick={beforeClick}> 이전 질문 </div>
+      </div>}
+
     </div>
   );
 };
