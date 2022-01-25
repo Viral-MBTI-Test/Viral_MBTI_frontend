19f2ff47e4375f3e85c434b30dd31089fd04cfb0
RightHennessy <myheaven_b@naver.com>
2022-01-25 19:23:35 +0900

    fix : friend list

--- src/query/queryend.tsx
+++ src/query/queryend.tsx
@@ -11,134 +11,136 @@ import { AxiosResponse } from "axios";
 import MBTIAnswer from "../share/MBTIAnswer";
 
 const QueryEnd = () => {
-    const navigate = useNavigate();
-    const [sameAns, setSameAns] = useState<any>([]);
-    const [sameMbti, setSameMbti] = useState<any>([]);
-    const [currentNo, setCurrentNo] = useState(0);
-    const [friendAns, setFriendAns] = useState<any>([]);
+  const navigate = useNavigate();
+  const [sameAns, setSameAns] = useState<any>([]);
+  const [sameMbti, setSameMbti] = useState<any>([]);
+  const [currentNo, setCurrentNo] = useState(0);
+  const [friendAns, setFriendAns] = useState<any>([]);
 
-    useEffect(() => {
-        const getAnswerStat = async () => {
-            const response: any = await webClient.get(
-                `/answer-statistics/?question=${currentNo + 1}`
-            );
+  useEffect(() => {
+    const getAnswerStat = async () => {
+      const response: any = await webClient.get(
+        `/answer-statistics/?question=${currentNo + 1}`
+      );
 
-            const response_mbti: any = await webClient.get(
-                `/question-statistics/?question=${currentNo + 1}`
-            );
+      const response_mbti: any = await webClient.get(
+        `/question-statistics/?question=${currentNo + 1}`
+      );
 
-            const response_friend: any = await webClient.get(
-                `/same-answer-friends/?question=${currentNo + 1}`
-            );
+      const response_friend: any = await webClient.get(
+        `/same-answer-friends/?question=${currentNo + 1}`
+      );
 
-            setSameAns(response.data);
-            setSameMbti(response_mbti.data);
-            setFriendAns(response_friend.data);
-        };
-        getAnswerStat();
-    }, [currentNo]);
-
-    const afterClick = () => {
-        if (currentNo === Querylist.length - 1) {
-            navigate("/result");
-        } else {
-            setCurrentNo((currentNo) => currentNo + 1);
-        }
+      setSameAns(response.data);
+      setSameMbti(response_mbti.data);
+      setFriendAns(response_friend.data);
     };
+    getAnswerStat();
+  }, [currentNo]);
 
-    const beforeClick = () => {
-        if (currentNo === 0) {
-            navigate("/result");
-        }
-        setCurrentNo((currentNo) => currentNo - 1);
-    };
+  const afterClick = () => {
+    if (currentNo === Querylist.length - 1) {
+      navigate("/result");
+    } else {
+      setCurrentNo((currentNo) => currentNo + 1);
+    }
+  };
 
-    return (
-        <div className="queryend_container">
-            <div className="progress_div"></div>
+  const beforeClick = () => {
+    if (currentNo === 0) {
+      navigate("/result");
+    }
+    setCurrentNo((currentNo) => currentNo - 1);
+  };
 
-            <div className="queryend_question">
-                {Querylist[currentNo].question}
-            </div>
+  return (
+    <div className="queryend_container">
+      <div className="progress_div"></div>
 
-            <div className="query_selected"> 선택한 답안 </div>
-            <div className="queryend_mbti"> 나와 같은 답을 선택한 MBTI </div>
+      <div className="queryend_question">{Querylist[currentNo].question}</div>
 
-            <MBTIPercent
-                index={0}
-                mbti={sameAns[0]?.mbti}
-                percent={sameAns[0]?.percent}
-            />
-            <MBTIPercent
-                index={1}
-                mbti={sameAns[1]?.mbti}
-                percent={sameAns[1]?.percent}
-            />
-            <MBTIPercent
-                index={2}
-                mbti={sameAns[2]?.mbti}
-                percent={sameAns[2]?.percent}
-            />
-            <MBTIPercent
-                index={3}
-                mbti={sameAns[3]?.mbti}
-                percent={sameAns[3]?.percent}
-            />
+      <div className="query_selected"> 선택한 답안 </div>
+      <div className="queryend_mbti"> 나와 같은 답을 선택한 MBTI </div>
 
-            <div className="queryend_mbti"> ENTP들은 이런 담을 골랐어요 </div>
+      <MBTIPercent
+        index={0}
+        mbti={sameAns[0]?.mbti}
+        percent={sameAns[0]?.percent}
+      />
+      <MBTIPercent
+        index={1}
+        mbti={sameAns[1]?.mbti}
+        percent={sameAns[1]?.percent}
+      />
+      <MBTIPercent
+        index={2}
+        mbti={sameAns[2]?.mbti}
+        percent={sameAns[2]?.percent}
+      />
+      <MBTIPercent
+        index={3}
+        mbti={sameAns[3]?.mbti}
+        percent={sameAns[3]?.percent}
+      />
 
-            <MBTIAnswer
-                index={0}
-                content={sameMbti[0]?.content}
-                percent={sameMbti[0]?.percent}
-            />
-            <MBTIAnswer
-                index={1}
-                content={sameMbti[1]?.content}
-                percent={sameMbti[1]?.percent}
-            />
-            <MBTIAnswer
-                index={2}
-                content={sameMbti[2]?.content}
-                percent={sameMbti[2]?.percent}
-            />
-            <MBTIAnswer
-                index={3}
-                content={sameMbti[3]?.content}
-                percent={sameMbti[3]?.percent}
-            />
+      <div className="queryend_mbti"> ENTP들은 이런 담을 골랐어요 </div>
 
-            <div className="queryend_mbti"> 나랑 같은 답을 선택한 친구들 </div>
+      <MBTIAnswer
+        index={0}
+        content={sameMbti[0]?.content}
+        percent={sameMbti[0]?.percent}
+      />
+      <MBTIAnswer
+        index={1}
+        content={sameMbti[1]?.content}
+        percent={sameMbti[1]?.percent}
+      />
+      <MBTIAnswer
+        index={2}
+        content={sameMbti[2]?.content}
+        percent={sameMbti[2]?.percent}
+      />
+      <MBTIAnswer
+        index={3}
+        content={sameMbti[3]?.content}
+        percent={sameMbti[3]?.percent}
+      />
 
-            <div>
+      <div className="queryend_mbti"> 나랑 같은 답을 선택한 친구들 </div>
+
+      <div>
+        {friendAns.length === 0 ? (
+          <MBTIProfile
+            friend_profile_image={undefined}
+            friend_result={undefined}
+            similar_percent={undefined}
+            friend_name={""}
+          />
+        ) : (
+          friendAns.map((friend: any, index: number) => {
+            if (index >= 3) return <></>;
+            else
+              return (
                 <MBTIProfile
-                    friend_profile_image={friendAns[0]?.friend_profile_image}
-                    friend_result={friendAns[0]?.friend_result}
-                    similar_percent={friendAns[0]?.percent}
-                    friend_name={friendAns[0]?.friend_name}
+                  friend_profile_image={friend?.friend_profile_image}
+                  friend_result={friend?.friend_result}
+                  similar_percent={friend?.percent}
+                  friend_name={friend?.friend_name}
+                  key={index}
                 />
-                <MBTIProfile
-                    friend_profile_image={friendAns[1]?.friend_profile_image}
-                    friend_result={friendAns[1]?.friend_result}
-                    similar_percent={friendAns[1]?.percent}
-                    friend_name={friendAns[1]?.friend_name}
-                />
-                <MBTIProfile
-                    friend_profile_image={friendAns[2]?.friend_profile_image}
-                    friend_result={friendAns[2]?.friend_result}
-                    similar_percent={friendAns[2]?.percent}
-                    friend_name={friendAns[2]?.friend_name}
-                />
-            </div>
+              );
+          })
+        )}
+      </div>
 
-            <div className="query_prevBtn" onClick={beforeClick}>
-                이전 문항
-            </div>
-            <div className="queryend_nextBtn" onClick={afterClick}>
-                다음 문항
-            </div>
-        </div>
-    );
+      <div className="query_prevBtn" onClick={beforeClick}>
+        이전 문항
+      </div>
+      <div className="queryend_nextBtn" onClick={afterClick}>
+        다음 문항
+      </div>
+    </div>
+  );
 };
 
 export default QueryEnd;
