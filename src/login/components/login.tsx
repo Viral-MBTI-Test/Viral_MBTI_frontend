af61c6a583be551f4589c73cb79afffdf8e8eff0
rlafl7942 <rlafl7942@naver.com>
2022-01-26 22:42:13 +0900

    Revert "add setMyMbti in queryEnd"
    This reverts commit 3afd641c6f9776c1130eb8a2b1b748f58f67d6a3.

--- src/login/components/login.tsx
+++ src/login/components/login.tsx
@@ -1,20 +1,19 @@
-import { useEffect, useState } from "react";
-import "../css/login.css";
-import boy from "../../images/boy.svg";
-import kakao_login_large_wide from "../../images/kakao_login_large_wide.png";
-import MbtiCube from "./mbtiCube";
-import vector from "../../images/vector.svg";
-import webClient from "../../share/webClient";
-import axios, { AxiosResponse } from "axios";
+import { useEffect, useState } from 'react';
+import '../css/login.css';
+import boy from '../../images/boy.svg';
+import kakao_login_large_wide from '../../images/kakao_login_large_wide.png';
+import MbtiCube from './mbtiCube';
+import vector from '../../images/vector.svg';
+import webClient from '../../share/webClient';
+import axios, { AxiosResponse } from 'axios';
 const { Kakao } = window;
 const loginWithKaKao = () => {
-    Kakao.Auth.authorize({
-        redirectUri: "https://localhost:3000/auth",
-    });
+  Kakao.Auth.authorize({
+    redirectUri: 'http://localhost:3000/auth',
+  });
 };
 
 const Login = () => {
-<<<<<<< Updated upstream
   const countParticipants = async () => {
     const participants: AxiosResponse = await axios.get(
       'https://mbti-test.duckdns.org/participants/'
@@ -46,54 +45,21 @@ const Login = () => {
         </div>
         <div className="boyImg">
           <img src={boy} alt="boy"></img>
-=======
-    const countParticipants = async () => {
-        const participants: AxiosResponse = await axios.get(
-            "https://mbti-test.duckdns.org/participants/"
-        );
-        setCount(participants.data.number_of_users);
-    };
-    useEffect(() => {
-        countParticipants();
-    }, []);
-    const [count, setCount] = useState<number>(0);
-    return (
-        <div className="login_container">
-            <div className="login_view">
-                <div className="login_mainTitle">
-                    <div>나는 정말</div>
-                    <div className="login_mbti">
-                        <MbtiCube index={0} />
-                        <MbtiCube index={1} />
-                        <MbtiCube index={2} />
-                        <MbtiCube index={3} />가 맞을까?
-                    </div>
-                </div>
-                <div className="login_subTitle">
-                    나와 같은 유형의 사람들은 정말 나와 비슷할까?
-                    <br />
-                    나랑 같은 MBTI를 가진 사람들은 어떤 생각을 가지고
-                    <br />
-                    있는지 확인해봐요.
-                </div>
-                <div className="boyImg">
-                    <img src={boy} alt="boy"></img>
-                </div>
-                <div className="login_countMsg">{`지금까지 ${count}명이 참여했어요`}</div>
-                <div>
-                    <img
-                        src={kakao_login_large_wide}
-                        alt="kakao login"
-                        className="kakao"
-                        onClick={() => {
-                            loginWithKaKao();
-                        }}
-                    ></img>
-                </div>
-            </div>
->>>>>>> Stashed changes
         </div>
-    );
+        <div className="login_countMsg">{`지금까지 ${count}명이 참여했어요`}</div>
+        <div>
+          <img
+            src={kakao_login_large_wide}
+            alt="kakao login"
+            className="kakao"
+            onClick={() => {
+              loginWithKaKao();
+            }}
+          ></img>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/share/webClient.tsx
+++ src/share/webClient.tsx
@@ -1,27 +1,23 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosInstance } from 'axios';
 
-<<<<<<< Updated upstream
 const API_ROOT = 'https://mbti-test.duckdns.org';
-=======
-const API_ROOT = "https://mbti-test.duckdns.org/";
->>>>>>> Stashed changes
 
 const webClient: AxiosInstance = axios.create({
-    baseURL: API_ROOT,
-    headers: {
-        Authorization: "Bearer " + localStorage.getItem("access"),
-    },
+  baseURL: API_ROOT,
+  headers: {
+    Authorization: 'Bearer ' + localStorage.getItem('access'),
+  },
 });
 
 webClient.interceptors.request.use(
-    (config: any) => {
-        const token = localStorage.getItem("access");
-        if (token) {
-            config.headers.Authorization = "Bearer " + token;
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
+  (config: any) => {
+    const token = localStorage.getItem('access');
+    if (token) {
+      config.headers.Authorization = 'Bearer ' + token;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
 export default webClient;
